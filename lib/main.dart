81ca7913f0dccb8c1ebb4d6a96acff85ceed5ab8
Dilshodbek0720 <sayitqulovdilshodbek@gmail.com>
2023-08-17 18:32:08 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -5,3 +5,27 @@
 // 3. Map task : 20 ball.
 // 4. Custom Paint and Animations: 20 ball.
 // 5. Suxbat: 20 ball
+
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Example"),
+        ),
+      )
+    );
+  }
+}
